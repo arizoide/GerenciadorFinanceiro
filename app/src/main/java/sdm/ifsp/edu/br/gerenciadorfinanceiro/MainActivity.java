0e412441f84e83a0d846768b5d8a43a7b84243fa
Ari Thomazini <ari.thomazini@gmail.com>
2018-12-12 18:52:53 -0200

    Initial commit

---
+++ app/src/main/java/sdm/ifsp/edu/br/gerenciadorfinanceiro/MainActivity.java
@@ -0,0 +1,30 @@
+package sdm.ifsp.edu.br.gerenciadorfinanceiro;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button cadastrarButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cadastrarButton = findViewById(R.id.cadastrarNovaContaButton);
+
+        cadastrarButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent i = new Intent(getApplicationContext(), CadastrarActivity.class);
+                        startActivityForResult(i, 1);
+                    }
+                }
+        );
+    }
+}
